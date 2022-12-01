4ece3e975c77997bb34f22cd3cc0210526ebed59
Teddy Okello <37796862+keystroke3@users.noreply.github.com>
2022-12-01 12:31:18 +0300

    Another year, another AoC; Day 1 submission

---
+++ 2022/day1/day1.py
@@ -0,0 +1,42 @@
+"""
+Advent of Code 2022 Day1
+"""
+from typing import TextIO
+
+# Part 1
+def highest_calory(f: TextIO) -> int:
+    calory_count = 0
+    top = 0
+    while True:
+        calories = f.readline()
+        if calories == "\n":
+            if calory_count > top:
+                top = calory_count
+            calory_count = 0
+            continue
+        elif calories == "":
+            return top
+        calory_count += int(calories)
+
+
+# Part 2
+def top3_calories(f: TextIO) -> int:
+    calory_count = 0
+    top1 = 0
+    top2 = 0
+    top3 = 0
+    while True:
+        calories = f.readline()
+        try:
+            calory_count += int(calories)
+        except ValueError:
+            if calories in ("\n", ""):
+                if calory_count > top1:
+                    top1, top2, top3 = calory_count, top1, top2
+                elif calory_count > top2:
+                    top2, top3 = calory_count, top2
+                elif calory_count > top3:
+                    top3 = calory_count
+                calory_count = 0
+            if calories == "":
+                return sum(top1, top2, top3)
